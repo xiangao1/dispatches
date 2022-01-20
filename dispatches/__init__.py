d436b491ec1debc223033760cbc6519ffd5e151b
Xian <xgao1@nd.edu>
2022-01-20 15:21:37 -0500

    demote the capabilities

--- dispatches/__init__.py
+++ dispatches/__init__.py
@@ -16,4 +16,3 @@ Workflow data management functionality.
 """
 # promote some things from the workflow.py module
 from .workflow import ManagedWorkflow
-from .prescient_result_analysis import NetworkData, PrescientSimulationData, Visualizer
